c6d39f4225bd8e532f6e27e2e91ed989addc53a7
aaolmedo2 <aaolmedo2@espe.edu.ec>
2024-02-29 19:20:39 -0500

    Agregando la paginacion

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,26 +1,44 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import {
+  TextField,
+  Button,
+  Typography,
+  Container,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://api.github.com/users/${username}/repos`
+          `https://api.github.com/users/${username}/repos?page=${page}&per_page=5`
         );
         const sortedRepos = response.data.sort((a, b) => b.size - a.size);
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        setRepos(sortedRepos);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
     };
 
     fetchData();
-  }, [username]);
+  }, [username, page]);
+
+  const handlePrevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
 
   return (
     <div>
@@ -32,6 +50,12 @@ const RepoList = ({ username }) => {
           </li>
         ))}
       </ul>
+      <div>
+        <Button variant="contained" onClick={handlePrevPage} disabled={page === 1}>
+          Anterior
+        </Button>
+        <Button variant="contained" color="secondary" onClick={handleNextPage}>Siguiente</Button>
+      </div>
     </div>
   );
 };
